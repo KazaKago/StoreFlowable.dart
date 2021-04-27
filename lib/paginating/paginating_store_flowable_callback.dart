c0daf4f111e5a23ff48c828c7156180a9aa70709
Kensuke Tamura <kazakago@gmail.com>
2021-04-27 21:24:07 +0900

    implement StoreFlowableCallback to PaginatingStoreFlowableCallback.

--- lib/paginating/paginating_store_flowable_callback.dart
+++ lib/paginating/paginating_store_flowable_callback.dart
@@ -1,11 +1,5 @@
-import 'package:store_flowable/flowable_data_state_manager.dart';
 import 'package:store_flowable/paginating/paginating_cache_data_manager.dart';
 import 'package:store_flowable/paginating/paginating_origin_data_manager.dart';
+import 'package:store_flowable/store_flowable_callback.dart';
 
-abstract class PaginatingStoreFlowableCallback<KEY, DATA> implements PaginatingCacheDataManager<DATA>, PaginatingOriginDataManager<DATA> {
-  KEY getKey();
-
-  FlowableDataStateManager<KEY> getFlowableDataStateManager();
-
-  Future<bool> needRefresh(final DATA cachedData);
-}
+abstract class PaginatingStoreFlowableCallback<KEY, DATA> implements StoreFlowableCallback<KEY, DATA>, PaginatingCacheDataManager<DATA>, PaginatingOriginDataManager<DATA> {}
--- lib/store_flowable_extension.dart
+++ lib/store_flowable_extension.dart
@@ -1,3 +1,6 @@
+import 'package:store_flowable/paginating/paginating_store_flowable.dart';
+import 'package:store_flowable/paginating/paginating_store_flowable_callback.dart';
+import 'package:store_flowable/src/paginating/paginating_store_flowable_impl.dart';
 import 'package:store_flowable/src/store_flowable_impl.dart';
 import 'package:store_flowable/store_flowable.dart';
 import 'package:store_flowable/store_flowable_callback.dart';
@@ -6,5 +9,12 @@ extension StoreFlowableExtension<KEY, DATA> on StoreFlowableCallback<KEY, DATA> 
   // ignore: use_to_and_as_if_applicable
   StoreFlowable<KEY, DATA> create() {
     return StoreFlowableImpl(this);
+  }
+}
+
+extension PaginatingStoreFlowableExtension<KEY, DATA> on PaginatingStoreFlowableCallback<KEY, DATA> {
+  // ignore: use_to_and_as_if_applicable
+  PaginatingStoreFlowable<KEY, DATA> create() {
+    return PaginatingStoreFlowableImpl(this);
   }
 }
